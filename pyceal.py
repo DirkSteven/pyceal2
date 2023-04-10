ec0fd8374281c7e80eb147e6dcdedf88cf0857ca
Dirk Steven E. Javier <dirkjaviermvp@gmail.com>
2023-04-11 00:18:01 +0800

    Fixed Links

--- pyceal.py
+++ pyceal.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , url_for
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def root_page():
-    return render_template ('root_page.html')
+    return "<h2> Root Page </h2>"
 
-# @app.route("/id_generator")
-# def id_gen():
-#      return render_template ('id_generator.html')
+@app.route("/id_generator")
+def id_gen():
+      return render_template ('id_generator.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
